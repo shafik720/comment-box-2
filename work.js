f7f7875bbca5b912d568915d3b399447c526575c
shafik720 <shafikrasel5@gmail.com>
2022-08-01 13:28:54 +0600

    xdfgdsg

--- work.js
+++ work.js
@@ -17,6 +17,17 @@ document.getElementById('btn').addEventListener('click',function(){
 
         let nameDiv = document.createElement('div');
         nameDiv.classList.add('d-flex', 'justify-content-between');
+        nameDiv.innerHTML = `
+            <span><a href="#"><i class=" text-danger fa-solid fa-trash-can"></i></a></span>
+        `;
+        nameDiv.appendChild(createName);
         
+        // comment box for packeting nameDiv & commentDiv together
+        let commentBox = document.createElement('div');
+        commentBox.classList.add('border','border-3','border-primary','py-3','px-3','rounded','my-3');
+        commentBox.appendChild(commentDiv);
+        commentBox.appendChild(nameDiv);
+
+        document.getElementById('mainBox').appendChild(commentBox);
     }
 })
